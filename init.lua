44f8a6a1eebb723355aec9752d85a1f2f68547b5
Drake Axelrod <drake@draxel.io>
2022-12-03 20:45:29 +0100

    lazy commit

--- init.lua
+++ init.lua
@@ -1,11 +1,14 @@
-require("tools")
-CONF.leader("<space>")
-CONF.colorscheme("onedarkpro")
-CONF.options("options")
-CONF.plugins("plugins")
-CONF.servers("servers")
-CONF.keymaps("keymaps")
-CONF.autocommands("autocommands")
-CONF.commands("commands")
-CONF.load()
-
+if vim.fn.exists("g:vscode") == 1 then
+  return
+else
+  require("tools")
+  CONF.leader("<space>")
+  CONF.colorscheme("onedarkpro")
+  CONF.options("options")
+  CONF.plugins("plugins")
+  CONF.servers("servers")
+  CONF.keymaps("keymaps")
+  CONF.autocommands("autocommands")
+  CONF.commands("commands")
+  CONF.load()
+end
